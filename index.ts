448925726baceefbf33d199eb12cfa4c561828ef
Nick Polizogopoulos <133910144+Nick-Polizogopoulos@users.noreply.github.com>
2023-05-18 00:22:10 +0300

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,13 @@
+
+fizzBuzz(10, 25)
+
+function fizzBuzz(numberOne: number, numberTwo: number): void {
+
+  for (let i = numberOne; i <= numberTwo; i++)
+    if (i % 3 === 0 && i % 5 === 0) console.log(`${i}: FizzBuzz!`);
+    else if (i % 3 === 0) console.log(`${i}: Fizz`);
+    else if (i % 5 === 0) console.log(`${i}: Buzz`);
+    else console.log(`${i}: -`);
+    
+}
+
