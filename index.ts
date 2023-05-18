74b35f7f173d272b155a301b0e34e573226fa7d1
Nick Polizogopoulos <133910144+Nick-Polizogopoulos@users.noreply.github.com>
2023-05-18 15:21:58 +0300

    index.ts

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ fizzBuzz(10, 25);
 
 function fizzBuzz(numberOne: number, numberTwo: number): void {
 
-  for (let i = numberOne; i <= numberTwo; i++)
+  for (let i: number = numberOne; i <= numberTwo; i++)
     if (i % 3 === 0 && i % 5 === 0) console.log(`${i}: FizzBuzz!`);
     else if (i % 3 === 0) console.log(`${i}: Fizz`);
     else if (i % 5 === 0) console.log(`${i}: Buzz`);
